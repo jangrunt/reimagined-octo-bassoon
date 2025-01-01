aff1532aba282979e7035ad389ba2d7f1cbccf70
Jan Grunt <jgrunt10@gmail.com>
2025-01-01 03:58:42 +0100

    before lib/config removal

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly userService: UsersService) { }
+
+    @Post()
+    async createUser(@Body() createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.userService.findAll();
+    }
+}
